13f6f458c737d8fac2fc82ac8581085cc6d9255c
Anne <anne12080901@gmail.com>
2025-01-25 20:10:42 -0500

    frontend ui

---
+++ my-vite-app/app/components/Goose.tsx
@@ -0,0 +1,20 @@
+
+
+// interface GooseProps {
+//   className?: string
+// }
+
+// export function Goose({ className = "" }: GooseProps) {
+//   return (
+//     <div className={`relative w-32 h-32 ${className}`}>
+//       <img
+//         src="./goose.png"
+//         alt="Goose character"
+//         className="object-contain"
+//       />
+//     </div>
+//   )
+// }
+
+
+  
