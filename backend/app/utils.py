7c3373582f170f530e6d6139f5fbe784227d0ce2
Yashas Jindal <yashasjindal@gmail.com>
2025-01-26 00:06:13 -0500

    [wip] integrating external API

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -2,8 +2,8 @@ from torchvision import transforms
 
 # Define transformations
 transform = transforms.Compose([
-    transforms.Resize((128,128)),
     transforms.Grayscale(num_output_channels=1),
+    transforms.Resize((128,128)),
     transforms.ToTensor(),
     transforms.Normalize((0.5,), (0.5,))
 ])
